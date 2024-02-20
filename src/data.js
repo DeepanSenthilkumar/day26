81d590c79153a52c0c5456734b05f64f696b6d43
DeepanSenthilkumar <deepansenthilk2000@gmail.com>
2024-02-20 12:08:58 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const userList = [
+    {
+      id: 1,
+      name: "Deepan",
+      username: "DeepanPd",
+    },
+    {
+      id: 2,
+      name: "Surya",
+      username: "DarkWolff",
+    },
+  ];
+  
+  export default userList;
+  
